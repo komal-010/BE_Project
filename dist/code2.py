d23c5acab689df9b8bd073fd5f346a1dff063d19
komal010 <bhosalekomal369@gmail.com>
2024-08-01 23:17:13 +0530

    python

---
+++ dist/code2.py
@@ -0,0 +1,29 @@
+x=2
+y=3.14
+z=-5j
+a=float(x)
+b=int(y)
+c=complex(z)
+print(a)
+print(b)
+print(c)
+
+a="Hello , world!"
+print(len(a))
+
+b=" Welcome , komal"
+print(a.strip())
+
+c = "Hello,world"
+print(c.lower())
+
+d = "Hello , World!"
+print(d.replace("H","K"))
+
+e = "Hello , World"
+print(e.split(","))
+
+f = "Hello"
+g = "World"
+h = f+" "+g
+print(h)
